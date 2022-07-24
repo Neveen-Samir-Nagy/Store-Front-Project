cbc3fed96102fc1f20a52b1016d16b55b73bb381
Neveen Samir <48647031+Neveen-Samir-Nagy@users.noreply.github.com>
2022-07-24 15:50:46 +0200

    push migration folder

---
+++ migrations/sqls/20220716151207-users-table-up.sql
@@ -0,0 +1,7 @@
+/* Replace with your SQL commands */
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(15),
+    lastName VARCHAR(15),
+    password TEXT
+);
---
+++ migrations/sqls/20220716153049-products-table-up.sql
@@ -0,0 +1,7 @@
+/* Replace with your SQL commands */
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    price INTEGER NOT NULL,
+    category VARCHAR(20)
+);
